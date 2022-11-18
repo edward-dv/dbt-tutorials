db001c49ef3622c3652299e54fa49ad2d3fdf637
edward-dv <edvp93@gmail.com>
2022-11-18 00:31:32 +0000

    Sources, tests and documentation

---
+++ tests/test_stg_payments.sql
@@ -0,0 +1,9 @@
+-- The total amount should always be >= 0.
+-- Therefore return records where this isn't true to make the test fail.
+select
+    order_id,
+    sum(amount) as total_amount
+from {{ ref('stg_payments') }}
+group by 1
+having not(total_amount >= 0)
+-- or having total_amount < 0)
